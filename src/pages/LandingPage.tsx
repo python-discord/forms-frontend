893bad80bca81f30edb4969ccda9a8db205f2855
Joe Banks <joseph@josephbanks.me>
2020-09-28 20:53:01 +0100

    Change landing page structure to be centered

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { css, jsx } from "@emotion/core";
 
 import HeaderBar from "../components/HeaderBar";
 import FormListing from "../components/FormListing";
@@ -7,13 +7,17 @@ import FormListing from "../components/FormListing";
 function LandingPage() {
   return <div>
     <HeaderBar/>
-    <div css={{marginLeft: "20px"}}>
-      <h1>Welcome to Python Discord Forms</h1>
+    <div>
 
-      <h3>Available forms</h3>
-
-      <FormListing title="Ban Appeals" description="Appealing bans from the Discord server" open={true}/>
-      <FormListing title="Insights 2020" description="Insights about the Python Discord community" open={false}/>
+      <div css={css`
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+      `}>
+        <h1>Available Forms</h1>
+        <FormListing title="Ban Appeals" description="Appealing bans from the Discord server" open={true}/>
+        <FormListing title="Insights 2020" description="Insights about the Python Discord community" open={false}/>
+      </div>
     </div>
   </div>
 }
