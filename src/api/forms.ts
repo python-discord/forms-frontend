9566564100448a78e6746a5dfcbd761e99ec31f7
Joe Banks <joseph@josephbanks.me>
2020-09-30 21:31:25 +0100

    Update form properties to mirror those in the form listing component

--- src/api/forms.ts
+++ src/api/forms.ts
@@ -1,5 +1,5 @@
 export interface Form {
-    name: string,
+    title: string,
     description: string,
     open: boolean
 }
@@ -7,17 +7,17 @@ export interface Form {
 export function getForms(): Form[] {
     return [
         {
-            name: "Ban Appeals",
+            title: "Ban Appeals",
             description: "Appealing bans from the Discord server",
             open: true
         },
         {
-            name: "Insights 2020",
+            title: "Insights 2020",
             description: "Insights about the Python Discord community",
             open: false
         },
         {
-            name: "Code Jam 2099 Sign Ups",
+            title: "Code Jam 2099 Sign Ups",
             description: "Signing up for Python Discord's millionth code jam!",
             open: false
         }
