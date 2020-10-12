00e00bfb9eeee2c404c385c1e844d3b1246c9407
Joe Banks <joseph@josephbanks.me>
2020-10-12 14:11:47 +0100

    Update build path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env) => {
     entry: "./src/index.tsx",
     mode: env,
     output: {
-      path: path.resolve(__dirname, "../build/js"),
+      path: path.resolve(__dirname, "build/js"),
       filename: "[name].bundle.js",
       publicPath: "/",
       devtoolModuleFilenameTemplate: "file:///" + path.resolve(__dirname, "[resource-path]?[loaders]")
