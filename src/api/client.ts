41dc7ef649227673fa0015a1acd923d3be33d470
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-14 21:17:10 +0200

    Simplify Axios client baseURL definition
    Co-authored-by: Joe Banks <joseph@josephbanks.me>

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: process.env.BACKEND_URL || "https://forms-api.pythondiscord.com/"
+    baseURL: process.env.BACKEND_URL
 })
