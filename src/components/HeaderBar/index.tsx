6aa3235acdba58b3d27dd9b8c40caafcfa37b574
Hassan Abouelela <47495861+HassanAbouelela@users.noreply.github.com>
2021-01-06 09:29:22 +0300

    Move Home Button
    Signed-off-by: Hassan Abouelela <47495861+HassanAbouelela@users.noreply.github.com>

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -5,7 +5,7 @@ import Header1 from "./header_1.svg";
 import Header2 from "./header_2.svg";
 import Logo from "./logo.svg";
 
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 interface HeaderBarProps {
     title?: string
@@ -22,27 +22,54 @@ const headerImageStyles = css`
   }
 `;
 
+const homeButtonStyles = css`
+  display: flex;
+  z-index: 0;
+  cursor: pointer;
+  position: absolute;
+
+  transform-origin: top left;
+
+  transform: scale(0.25);
+  transition: transform 300ms;
+
+  @media (max-width: 900px) {
+    transform: scale(0.2);
+  }
+
+  @media (max-width: 480px) {
+    top: 0;
+    width: 500vw; /* 100% // 0.2 (scaling) */
+    justify-content: center;
+  }
+`;
+
 const headerTextStyles = css`
   transition: margin 1s;
   font-family: "Uni Sans", "Hind", "Arial", sans-serif;
 
-  margin: 0 2rem 10rem 2rem;
-  
+  margin: 0 2rem 10rem 7rem;
+
   .title {
     font-size: 3vmax;
     margin-bottom: 0;
   }
-  
+
   .description {
     font-size: 1.5vmax;
   }
-  
+
   .title, .description {
     transition: font-size 1s;
   }
 
+  @media (max-width: 900px) {
+    margin-left: 6rem;
+  }
+
   @media (max-width: 480px) {
     margin-top: 7rem;
+    margin-left: 2rem;
     text-align: center;
 
     .title {
@@ -55,35 +82,8 @@ const headerTextStyles = css`
   }
 `;
 
-const homeButtonStyles = css`
-  svg {
-    transform: scale(0.25);
-    transition: top 300ms, transform 300ms;
-
-    @media (max-width: 480px) {
-      transform: scale(0.15);
-    }
-  }
-  
-  * {
-    position: absolute;
-    top: -10rem;
-    right: 1rem;
-
-    z-index: 0;
-    transform-origin: right;
-
-    @media (max-width: 700px) {
-      top: -11.5rem;
-    }
-
-    @media (max-width: 480px) {
-      top: -12.5rem;
-    }
-  }
-`;
-
 function HeaderBar({ title, description }: HeaderBarProps): JSX.Element {
+    const history = useHistory();
     if (!title) {
         title = "Python Discord Forms";
     }
@@ -95,14 +95,14 @@ function HeaderBar({ title, description }: HeaderBarProps): JSX.Element {
                 <Header2/>
             </div>
 
+            <div css={homeButtonStyles} onClick={() => history.push("/")}>
+                <Logo/>
+            </div>
+
             <div css={css`${headerTextStyles}; margin-bottom: 12.5%;`}>
                 <h1 className="title">{title}</h1>
                 <h1 className="description">{description}</h1>
             </div>
-
-            <Link to="/" css={homeButtonStyles}>
-                <Logo/>
-            </Link>
         </div>
     );
 }
