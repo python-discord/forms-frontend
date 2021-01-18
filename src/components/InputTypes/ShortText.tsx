2d7c77048609f97e0b94d7cb0d8c859a95e07285
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-18 17:58:01 +0200

    Implement validation to ShortText component

--- src/components/InputTypes/ShortText.tsx
+++ src/components/InputTypes/ShortText.tsx
@@ -1,13 +1,18 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import React, { ChangeEvent } from "react";
-import { textInputs } from "../../commonStyles";
+import React, { ChangeEvent, FocusEvent } from "react";
+import { textInputs, invalidStyles } from "../../commonStyles";
 
 interface ShortTextProps {
     handler: (event: ChangeEvent<HTMLInputElement>) => void,
-    required: boolean
+    blurHandler: (event: FocusEvent<HTMLInputElement>) => void,
+    valid: boolean
 }
 
 export default function ShortText(props: ShortTextProps): JSX.Element {
-    return <input type="text" css={textInputs} placeholder="Enter Text..." onChange={props.handler}/>;
+    return (
+        <div css={invalidStyles}>
+            <input type="text" css={textInputs} placeholder="Enter Text..." onChange={props.handler} onBlur={props.blurHandler} className={!props.valid ? "invalid-box" : ""}/>
+        </div>
+    );
 }
