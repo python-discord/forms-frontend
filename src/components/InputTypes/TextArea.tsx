df4cac518827b69977cdb156394abb399ef64871
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-18 11:35:00 +0200

    Display invalid information for TextArea

--- src/components/InputTypes/TextArea.tsx
+++ src/components/InputTypes/TextArea.tsx
@@ -2,10 +2,14 @@
 import { jsx, css } from "@emotion/react";
 import React, { ChangeEvent } from "react";
 import { textInputs } from "../../commonStyles";
+import ErrorMessage from "../ErrorMessage";
+import colors from "../../colors";
 
 interface TextAreaProps {
     handler: (event: ChangeEvent<HTMLTextAreaElement>) => void,
-    required: boolean
+    required: boolean,
+    valid: boolean,
+    error: string
 }
 
 const styles = css`
@@ -17,6 +21,18 @@ const styles = css`
   padding: 1rem;
 `;
 
+const invalidStyles = css`
+  .invalid-box {
+    box-shadow: 0 0 10px ${colors.error};
+    border: none;
+  }
+`;
+
 export default function TextArea(props: TextAreaProps): JSX.Element {
-    return <textarea css={[textInputs, styles]} placeholder="Enter Text..." onChange={props.handler}/>;
+    return (
+        <div css={invalidStyles}>
+            <textarea css={[textInputs, styles]} placeholder="Enter Text..." onChange={props.handler} className={!props.valid ? "invalid-box" : ""} required={props.required}/>
+            <ErrorMessage show={!props.valid} message={props.error}/>
+        </div>
+    );
 }
