6c88ec320843b1cfa461539c77592251db7d06cb
ks129 <45097959+ks129@users.noreply.github.com>
2021-02-05 17:41:15 +0200

    Toggle visibility instead returning null when no error should be shown

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -7,17 +7,14 @@ interface ErrorMessageProps {
     message: string
 }
 
-const styles = css`
-  color: ${colors.error};
-  font-size: 1.15rem;
-  line-height: 1.1rem;
-  margin: 1rem 0 0;
-`;
-
 export default function ErrorMessage(props: ErrorMessageProps): JSX.Element | null {
-    if (!props.show) {
-        return null;
-    }
+    const styles = css`
+      color: ${colors.error};
+      font-size: 1.15rem;
+      line-height: 1.1rem;
+      margin: 1rem 0 0;
+      visibilty: ${props.show ? "visible" : "hidden"}
+    `;
 
     return (
         <p css={styles}>{props.message}</p>
